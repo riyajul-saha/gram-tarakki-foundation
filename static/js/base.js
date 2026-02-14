a1884733239fb6fba1e433a802f7bfede463deba
riyajul-saha <riyajsaha879@hotmail.com>
2026-02-14 11:02:53 +0530

    Seperate Common code and Create a Separate file called as base file

---
+++ static/js/base.js
@@ -0,0 +1,19 @@
+// Mobile Navigation Toggle
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+const hamburgerIcon = hamburger.querySelector('i');
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+    hamburgerIcon.classList.toggle('fa-bars');
+    hamburgerIcon.classList.toggle('fa-times');
+});
+
+// Close mobile menu when clicking on a link
+document.querySelectorAll('.nav-menu a').forEach(link => {
+    link.addEventListener('click', () => {
+        navMenu.classList.remove('active');
+        hamburgerIcon.classList.add('fa-bars');
+        hamburgerIcon.classList.remove('fa-times');
+    });
+});
